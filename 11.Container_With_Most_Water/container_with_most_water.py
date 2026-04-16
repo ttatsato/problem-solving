c29d44839af2023ce980e91cac78b93fd0e2f488
ttatsato <t-satoh@alby.co.jp>
2026-04-16 11:35:00 +0900

    Create container_with_most_water.py

---
+++ 11.Container_With_Most_Water/container_with_most_water.py
@@ -0,0 +1,21 @@
+# 模範解答: 双方向ポインタ 縮小法
+# 29.77MB Beats19.58%
+# Runtime 59ms | Beats 52.14%
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_water = 0
+        left = 0
+        right = len(height) - 1
+        
+        while left < right:
+            # 低い方の高さ * 底辺の長さ
+            water = min(height[left], height[right]) * (right - left)
+            max_water = max(max_water, water)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -=1
+        
+        return max_water
